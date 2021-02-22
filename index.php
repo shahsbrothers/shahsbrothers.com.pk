578f49b41924e08850a2c3ec662bd324359e51ce
Khaalidi <developer@shahsbrothers.com.pk>
2021-02-22 12:45:46 +0500

    dynamic content a

--- index.php
+++ index.php
@@ -244,7 +244,7 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-6">
-                        <img src="assets/img/about.jpg" class="img-fluid" alt="">
+                        <img src="assets/img/about_1.png" class="img-fluid" alt="">
                     </div>
                     <div class="col-lg-6 pt-4 pt-lg-0 content">
                         <p class="text-justify">
