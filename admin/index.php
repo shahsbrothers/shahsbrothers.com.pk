7902475960addb0fd22670c1673d0821d2943ab8
Habib Ullah <habibullahkhaalidi@gmail.com>
2021-03-13 18:02:47 +0500

    Implement Users Permissions

--- admin/index.php
+++ admin/index.php
@@ -70,7 +70,7 @@ $userId = (isset($_SESSION['id']))?$_SESSION['id']:'';
 
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="index.php">
-            <Strong> </span>Shah's Brothers  <?php echo $userId;?> </Strong>
+            <Strong> </span>Shah's Brothers</Strong>
         </a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -222,7 +222,7 @@ $userId = (isset($_SESSION['id']))?$_SESSION['id']:'';
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4" style="display: none;" id="products">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h5"> Dashboard / Products</h1>
-                    <a href="#" class="btn btn-dark btn-sm" data-toggle="modal" data-target="#add_product" data-backdrop="static" data-keyboard="false"> <i class="fas fa-plus-circle"> </i> </a>
+                    <a href="#" class="btn btn-dark btn-sm" data-toggle="modal" data-target="#add_product" data-backdrop="static" data-keyboard="false"> <i class="fas fa-plus"> </i> </a>
                 </div>
                 <div>
 
